007ba1a6febaa134d7c5c9b7f93423c980740179
Filip Szczesniak <fiszczu@gmail.com>
2020-09-06 18:42:57 +0200

    create test file with displaying of computation results

---
+++ test/exampleMatrices.jl
@@ -0,0 +1,9 @@
+using LinearAlgebra
+
+matrix5x5 = Symmetric(rand(5, 5) * 100)
+
+matrix25x25 = Symmetric(rand(25, 25) * 100)
+
+matrix75x75 = Symmetric(rand(75, 75) * 100)
+
+matrix150x150 = Symmetric(rand(150, 150) * 100)
--- utils/verifyEigenvalues.jl
+++ utils/verifyEigenvalues.jl
@@ -20,14 +20,3 @@ function verifyEigenvectors(entryMatrix::Matrix{Float64}, eigenvectors::Array{Ve
         end
     end
 end
-
-A = [2.0 3.0 5.0; 3.0 8.0 1.0; 5.0 1.0 3.0]
-# First three element = correct, another three = wrong
-eigenvalues = [10.628, 5.137, -2.766, 43, 112, -3]
-# First element = correct, another one = wrong
-eigenvectors = [[-0.760; 0.153; 0.632], [32.0; 64.0; 22.0]]
-
-println("Verification for eigenvectors:")
-verifyEigenvalues(A, eigenvalues)
-println("\nVerification for eigenvectors:")
-verifyEigenvectors(A, eigenvectors)
