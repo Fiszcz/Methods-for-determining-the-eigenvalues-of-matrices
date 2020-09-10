0c2b3d01fdc044ed6b8089d8ff36b39535daab33
Filip Szczesniak <fiszczu@gmail.com>
2020-09-10 19:41:18 +0200

    improvements for Jacobi method

---
+++ jacobi_method/jacobiMethod.test.jl
@@ -0,0 +1,23 @@
+include("../utils/verifyEigenvalues.jl")
+include("./jacobiMethod.jl")
+using LinearAlgebra
+using BenchmarkTools
+using InteractiveUtils
+import Random
+
+Random.seed!(100)
+const A = Array(Symmetric(rand(5, 5) * 100))
+
+(eigenvalues, eigenvectors) = jacobiMethod(A)
+
+display(@code_warntype jacobiMethod(A))
+
+display(eigvals(A))
+
+display(eigenvectors)
+
+println(eigenvalues)
+
+Random.seed!(100)
+const B = Array(Symmetric(rand(15, 15)))
+display(@benchmark jacobiMethod(B))
