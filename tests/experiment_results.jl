d683ad6fb789d55a2aa190441c2038d80872b0a5
Arosos <a.wierzbicki96@gmail.com>
2020-09-10 23:01:26 +0200

    Added tests for checking for correctness of eigenvalues and eigenvectors.

--- tests/experiment_results.jl
+++ tests/experiment_results.jl
@@ -1,6 +1,7 @@
 include("../jacobi_method/jacobiMethod.jl")
 include("../power_method/powerMethod.jl")
 include("../qr_method/qrMethod.jl")
+include("../utils/verifyEigenvalues.jl")
 using LinearAlgebra
 
 function eigenvalues_from_LinearAlgebra(entryMatrix)
@@ -11,20 +12,25 @@ function eigenvalues_from_LinearAlgebra(entryMatrix)
     println("Eigenvalues: ", values)
     println("Eigenvectors: ")
     display(vectors)
+    println()
 end
 
-function test_results_of_jacobi_method(entryMatrix)
-    (eigenValue, eigenVector) = jacobiMethod(entryMatrix)
+function test_results_of_jacobi_method(entryMatrix::Array{Float64, 2})
+    (eigenValues, eigenVectors) = jacobiMethod(entryMatrix)
 
     printstyled("JACOBI METHOD:\n", color = :(green))
 
     println("Computed values:")
-    println("Eigenvalue: ", eigenValue)
+    println("Eigenvalue: ", eigenValues)
     println("Eigenvector: ")
-    display(eigenVector)
+    display(eigenVectors)
+    println()
+    
+    verifyEigenvalues(entryMatrix, eigenValues)
+    verifyEigenvectors(entryMatrix, eigenVectors)
 end
 
-function test_results_of_power_method(entryMatrix)
+function test_results_of_power_method(entryMatrix::Array{Float64, 2})
     (eigenValues, eigenVectors) = powerMethodWithDeflation(entryMatrix)
 
     printstyled("POWER METHOD WITH DEFLATION:\n", color = :(green))
@@ -33,9 +39,13 @@ function test_results_of_power_method(entryMatrix)
     println("Eigenvalue: ", eigenValues)
     println("Eigenvector: ")
     display(eigenVectors)
+    println()
+
+    verifyEigenvalues(entryMatrix, eigenValues)
+    verifyEigenvectors(entryMatrix, eigenVectors)
 end
 
-function test_results_of_qr_method(entryMatrix)
+function test_results_of_qr_method(entryMatrix::Array{Float64, 2})
     (eigenValues, eigenVectors) = qrMethod(entryMatrix)
 
     printstyled("QR METHOD\n", color = :(green))
@@ -44,14 +54,22 @@ function test_results_of_qr_method(entryMatrix)
     println("Eigenvalues: ", eigenValues)
     println("Eigenvectors: ")
     display(eigenVectors)
+    println()
+
+    verifyEigenvalues(entryMatrix, eigenValues)
+    verifyEigenvectors(entryMatrix, eigenVectors)
 end
 
-const entryMatrix = Array(Symmetric(rand(4, 4)))
+function tests()
+    entryMatrix::Array{Float64, 2} = Array(Symmetric(rand(4, 4)))
 
-eigenvalues_from_LinearAlgebra(entryMatrix)
-println()
-test_results_of_jacobi_method(entryMatrix)
-println()
-test_results_of_power_method(entryMatrix)
-println()
-test_results_of_qr_method(entryMatrix)
+    eigenvalues_from_LinearAlgebra(entryMatrix)
+    println()
+    test_results_of_jacobi_method(entryMatrix)
+    println()
+    test_results_of_power_method(entryMatrix)
+    println()
+    test_results_of_qr_method(entryMatrix)
+end
+
+tests()
--- utils/verifyEigenvalues.jl
+++ utils/verifyEigenvalues.jl
@@ -1,18 +1,19 @@
-function verifyEigenvalues(entryMatrix::Matrix{Float64}, eigenvalues::Vector{Float64})
+function verifyEigenvalues(entryMatrix::Array{Float64, 2}, eigenvalues::Vector{Float64})
     for eigenvalue in eigenvalues
         determinant = det(entryMatrix - eigenvalue*I)
         if determinant < 0.05 && determinant > -0.05
             println("Correct eigenvalue: ", eigenvalue)
         else
             println("Wrong eigenvalue: ", eigenvalue)
-            println("Determinant: ", determinant)
         end
     end
 end
 
-function verifyEigenvectors(entryMatrix::Matrix{Float64}, eigenvectors::Array{Vector{Float64}})
-    eigenvalues = eigvals(A)
-    for eigenvector in eigenvectors
+function verifyEigenvectors(entryMatrix::Array{Float64, 2}, eigenvectors::Array{Float64, 2})
+    eigenvalues = eigvals(entryMatrix)
+    (n, n) = size(entryMatrix)
+    for i = 1:n
+        eigenvector = eigenvectors[:,i]
         if any(eigenvalue -> all(element -> element > -0.05 && element < 0.05, (entryMatrix - eigenvalue*I)*eigenvector), eigenvalues)
             println("Correct eigenvector: ", eigenvector)
         else
